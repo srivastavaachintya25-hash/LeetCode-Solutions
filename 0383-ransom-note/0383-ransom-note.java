c7e3c63a6dd35add89bda8953a7947a75f798e4a
Achintya Srivastava <srivastavaachintya25@gmail.com>
2026-09-08 12:10:57 +0530

    Time: 1 ms (99.95%), Space: 46.5 MB (57.9%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            count[magazine.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            int charIndex = ransomNote.charAt(i) - 'a';
+            count[charIndex]--;
+            
+            if (count[charIndex] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
