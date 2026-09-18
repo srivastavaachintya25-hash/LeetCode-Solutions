28c148c9b68f0151ffb1fa8ec83e074c4290a483
Achintya Srivastava <srivastavaachintya25@gmail.com>
2026-09-18 15:33:53 +0530

    Time: 4 ms (83.68%), Space: 46.7 MB (8.91%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] S = s.toCharArray();
+        char[] T = t.toCharArray();
+        Arrays.sort(S);
+        Arrays.sort(T);
+        return Arrays.equals(S,T); 
+        
+    }
+}
