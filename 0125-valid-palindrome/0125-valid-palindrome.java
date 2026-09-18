beb603b8b6836dca2f0d2cad7adbe3e5c304b2ca
Achintya Srivastava <srivastavaachintya25@gmail.com>
2026-09-18 14:41:18 +0530

    Time: 2 ms (99.31%), Space: 44 MB (91.02%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s == null) return false;
+        
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            char cLeft = s.charAt(left);
+            char cRight = s.charAt(right);
+            
+            if (!Character.isLetterOrDigit(cLeft)) {
+                left++;
+            } 
+
+            else if (!Character.isLetterOrDigit(cRight)) {
+                right--;
+            } 
+            else {
+                if (Character.toLowerCase(cLeft) != Character.toLowerCase(cRight)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        
+        return true;
+    }
+}
